5c0fe41898acb720a10d28daf585daaecd931173
kpotapovich <kris.potapovich@gmail.com>
2021-03-15 23:48:11 +0300

    init: function for checking given parentheses

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let buff = [];
+    let arr = str.split('');
+    for (let i = 0; i < arr.length; i++) {
+        let item = arr[i];
+
+        let brackets = bracketsConfig.find(v => v.indexOf(item) >= 0);
+        if (brackets) {
+            let idx = brackets.lastIndexOf(item);
+            if (idx === 1) {
+                if (buff.length > 0 &&
+                    buff[buff.length - 1] !== brackets[1] &&
+                    brackets[0] === brackets[1]) {
+
+                    buff.push(item);
+                } else {
+                    let k = buff.pop();
+                    if (k !== item) {
+                        return false;
+                    }
+                }
+
+            }
+            if (idx === 0) {
+                buff.push(item);
+            }
+
+        }
+    }
+    return buff.length === 0;
 }
